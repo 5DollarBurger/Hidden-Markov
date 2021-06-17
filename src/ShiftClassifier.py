1a6a993663a716ba4df8677400176ea77b9d0e4e
Darrell Leong <a843183@yara.com>
2021-06-17 23:04:36 +0800

    completed shift classifier with method of moments

--- src/ShiftClassifier.py
+++ src/ShiftClassifier.py
@@ -1,4 +1,5 @@
 from src.HMMDiscrete import HMMDiscrete
+import math
 import os
 import json
 
@@ -24,6 +25,18 @@ class ShiftClassifier:
         labels = tuple(aveStgDur * x for x in range(-numShifts, numShifts + 1)) #* aveStgDur
         return labels
 
+    def _getMean(self, histDict):
+        mu = 0
+        for shiftWk, prob in histDict.items():
+            mu += prob * shiftWk
+        return mu
+
+    def _getStd(self, histDict, mu):
+        var = 0
+        for shiftWk, prob in histDict.items():
+            var += prob * ((shiftWk - mu) ** 2)
+        return math.sqrt(var)
+
     def getShiftHist(self, X, shiftRange="single"):
         hmmParams = self.confDict["defaultParams"][shiftRange]
 
@@ -37,6 +50,12 @@ class ShiftClassifier:
             histDict[shift] = prob
 
         return histDict
+
+    def getShiftDist(self, X, shiftRange="single"):
+        histDict = self.getShiftHist(X=X, shiftRange=shiftRange)
+        mu = self._getMean(histDict=histDict)
+        sigma = self._getStd(histDict=histDict, mu=mu)
+        return {"histDict": histDict, "mu": mu, "sigma": sigma}
 
 
 if __name__ == "__main__":
@@ -52,6 +71,17 @@ if __name__ == "__main__":
     ]
     X_single = [[1, 1, 2, 2, 2, 1]]
     ins = ShiftClassifier(cropCal=cropCal)
-    histDict = ins.getShiftHist(X=X_single)
-    plt.bar(x=histDict.keys(), height=histDict.values())
+    distDict = ins.getShiftDist(X=X_single)
+    histDict = distDict["histDict"]
+    # plt.bar(x=histDict.keys(), height=histDict.values())
+
+    import scipy.stats
+    import numpy as np
+    aveStgDur = ins._getAveStgDur()
+    x = np.linspace(-2*aveStgDur, 2*aveStgDur, 100) * 7
+    y = scipy.stats.norm.pdf(x, distDict["mu"]*7, distDict["sigma"]*7)
+    plt.plot(x, y, color="red")
+    plt.xlabel("Shift from Calendar (days)")
+    plt.ylabel("Probability Density")
+
     plt.show()
