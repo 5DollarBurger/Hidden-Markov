84cf5579cafef7d5bf7779f4b979004440356c39
Darrell Leong <a843183@yara.com>
2021-06-18 07:18:59 +0800

    completed ref stg id

---
+++ src/StageClassifier.py
@@ -0,0 +1,72 @@
+import math
+
+
+class StageClassifier:
+    def __init__(self, cropCal, dayInd):
+        self.cropCal = cropCal
+        self.dayInd = dayInd
+        self.stgInd = self._getRefStage()
+
+    def _getAveStgDur(self):
+        totalDur = 0
+        for stgObj in self.cropCal:
+            totalDur += stgObj["weeks"]
+        return totalDur / len(self.cropCal)
+
+    def _getMean(self, histDict):
+        mu = 0
+        for shiftWk, prob in histDict.items():
+            mu += prob * shiftWk
+        return mu
+
+    def _getStd(self, histDict, mu):
+        var = 0
+        for shiftWk, prob in histDict.items():
+            var += prob * ((shiftWk - mu) ** 2)
+        return math.sqrt(var)
+
+    def _getRefStage(self):
+        stgInd = 0
+        sumDays = 0
+        for stg in self.cropCal:
+            sumDays += stg["days"]
+            if self.dayInd >= sumDays:
+                stgInd += 1
+            else:
+                break
+        return stgInd
+
+    def _getStageProgress(self):
+        sumDays = 0
+        for stg in self.cropCal[:self.stgInd]:
+            sumDays += stg["days"]
+        daysInStg = self.dayInd + 1 - sumDays
+        return daysInStg / self.cropCal[self.stgInd]["days"]
+
+    def _getHiddenShiftDays(self, shiftHist):
+        ...
+
+    def getStageHist(self, shiftHist, meanShift, stdShift):
+        ...
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    cropCal = [
+        {"stage": "seedling", "days": 28},
+        {"stage": "tillering", "days": 42},
+        {"stage": "panicle", "days": 21},
+        {"stage": "flowering", "days": 14},
+        {"stage": "filling", "days": 21},
+        {"stage": "maturity", "days": 14}
+    ]
+
+    shiftHist = {
+        -1: 0.08438500793171773,
+        0: 0.9156149920682819,
+        1: 2.3559965023904594e-16
+    }
+
+    ins = StageClassifier(cropCal=cropCal, dayInd=13)
+    print(ins._getStageProgress())
