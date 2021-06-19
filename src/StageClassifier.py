934d8a3b309e5a9830858d767b9c1aa1868ee186
Darrell Leong <a843183@yara.com>
2021-06-19 12:34:15 +0800

    changed stage classifier to accept time instead of stage shifts

--- src/StageClassifier.py
+++ src/StageClassifier.py
@@ -2,55 +2,49 @@ import math
 
 
 class StageClassifier:
-    def __init__(self, cropCal, dayInd):
+    def __init__(self, cropCal, weekInd):
         self.cropCal = cropCal
-        self.dayInd = dayInd
-        self.stgInd = self._getRefStage()
+        self.weekInd = weekInd
 
-    def _getAveStgDur(self):
-        totalDur = 0
+    # def _getRefStage(self):
+    #     stgInd = 0
+    #     sumWeeks = 0
+    #     for stg in self.cropCal:
+    #         sumWeeks += stg["weeks"]
+    #         if self.weekInd >= sumWeeks:
+    #             stgInd += 1
+    #         else:
+    #             break
+    #     return stgInd
+
+    # def _getStageProgress(self):
+    #     sumDays = 0
+    #     for stg in self.cropCal[:self.stgInd]:
+    #         sumDays += stg["days"]
+    #     daysInStg = self.dayInd + 1 - sumDays
+    #     return daysInStg / self.cropCal[self.stgInd]["days"]
+
+    def _getStgWkList(self):
+        stgWkList = []
         for stgObj in self.cropCal:
-            totalDur += stgObj["weeks"]
-        return totalDur / len(self.cropCal)
+            for wk in range(stgObj["weeks"]):
+                stgWkList.append(stgObj["stage"])
+        return stgWkList
 
-    def _getMean(self, histDict):
-        mu = 0
-        for shiftWk, prob in histDict.items():
-            mu += prob * shiftWk
-        return mu
+    def getStageHist(self, weekShiftHist):
+        # get list of stages across weeks
+        stgWkList = self._getStgWkList()
 
-    def _getStd(self, histDict, mu):
-        var = 0
-        for shiftWk, prob in histDict.items():
-            var += prob * ((shiftWk - mu) ** 2)
-        return math.sqrt(var)
-
-    def _getRefStage(self):
-        stgInd = 0
-        sumDays = 0
-        for stg in self.cropCal:
-            sumDays += stg["days"]
-            if self.dayInd >= sumDays:
-                stgInd += 1
-            else:
-                break
-        return stgInd
-
-    def _getStageProgress(self):
-        sumDays = 0
-        for stg in self.cropCal[:self.stgInd]:
-            sumDays += stg["days"]
-        daysInStg = self.dayInd + 1 - sumDays
-        return daysInStg / self.cropCal[self.stgInd]["days"]
-
-    def getStageHist(self, stgShiftHist):
+        # initialise stage histogram dict
         stgHist = {}
-        for ind in range(len(self.cropCal)):
-            stgHist[ind] = 0
+        for stgObj in self.cropCal:
+            stgHist[stgObj["stage"]] = 0
 
-        maxStgInd = len(self.cropCal) - 1
-        for stgShift, prob in stgShiftHist.items():
-            stg = min(max(self.stgInd + stgShift, 0), maxStgInd)
+        # fill in stage probabilities
+        maxWkInd = len(stgWkList) - 1
+        for wkShift, prob in weekShiftHist.items():
+            wkInd = min(max(round(self.weekInd + wkShift), 0), maxWkInd)
+            stg = stgWkList[wkInd]
             stgHist[stg] += prob
         return stgHist
 
@@ -60,19 +54,19 @@ if __name__ == "__main__":
     import json
 
     cropCal = [
-        {"stage": "seedling", "days": 28},
-        {"stage": "tillering", "days": 42},
-        {"stage": "panicle", "days": 21},
-        {"stage": "flowering", "days": 14},
-        {"stage": "filling", "days": 21},
-        {"stage": "maturity", "days": 14}
+        {"stage": "seedling", "weeks": 4},
+        {"stage": "tillering", "weeks": 6},
+        {"stage": "panicle", "weeks": 3},
+        {"stage": "flowering", "weeks": 2},
+        {"stage": "filling", "weeks": 3},
+        {"stage": "maturity", "weeks": 2}
     ]
 
-    stgShiftHist = {
-        -1: 0.08438500793171773,
-        0: 0.9156149920682819,
-        1: 2.3559965023904594e-16
+    weekShiftHist = {
+        -3.3333333333333335: 0.2,
+        0.0: 0.5,
+        3.3333333333333335: 0.3
     }
 
-    ins = StageClassifier(cropCal=cropCal, dayInd=0)
-    print(json.dumps(ins.getStageHist(stgShiftHist=stgShiftHist), indent=4))
+    ins = StageClassifier(cropCal=cropCal, weekInd=8)
+    print(json.dumps(ins.getStageHist(weekShiftHist=weekShiftHist), indent=4))
