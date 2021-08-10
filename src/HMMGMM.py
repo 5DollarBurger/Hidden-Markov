20ae55c1036277df0100a4958bb97e1ac00f524a
Darrell Leong <a843183@yara.com>
2021-08-10 17:41:30 +0800

    calculate alpha in main it

--- src/HMMGMM.py
+++ src/HMMGMM.py
@@ -1,10 +1,11 @@
 import numpy as np
+from scipy.stats import multivariate_normal as mvn
 
 
 class HMMGMM:
     def __init__(self, M, K):
         self.M = M
-        self.K = K
+        self.K = K  # number of Gaussians
 
     def _getFormattedx(self, x):
         """
@@ -32,13 +33,21 @@ class HMMGMM:
         arr = np.random.random(shape)
         return arr / arr.sum(axis=1, keepdims=True)
 
+    def _getAlpha(self, x, B):
+        T = len(x)
+        alpha = np.zeros(shape=(T, self.M))
+        alpha[0] = self.pi * B[:, 0]
+        for t in range(1, T):
+            alpha[t] = alpha[t-1].dot(self.A) * B[:, t]
+        return alpha
+
     def _setInitialParams(self, X):
         N = len(X)  # number of observations
         self.pi = np.ones(self.M) / self.M
         self.A = self._getRandomNormalized(shape=(self.M, self.M))
         self.R = np.ones((self.M, self.K)) / self.K  # responsibility
 
-        self.mu = np.zeros((self.M, self.K, self.D))
+        self.mu = np.zeros(shape=(self.M, self.K, self.D))
         for i in range(self.M):
             for k in range(self.K):
                 random_idx = np.random.choice(N)
@@ -46,12 +55,48 @@ class HMMGMM:
                 random_time_idx = np.random.choice(len(x))
                 self.mu[i, k] = x[random_time_idx]
 
-        self.sigma = np.zeros((self.M, self.K, self.D, self.D))
+        self.sigma = np.zeros(shape=(self.M, self.K, self.D, self.D))
         for j in range(self.M):
             for k in range(self.K):
                 self.sigma = np.eye(N=self.D)
 
-    def fit(self, X, max_iter=30):
+    def _setParams(self, X, max_iter=30):
+        # training data characteristics
+        N = len(X)
+
+        # update pi, A, B
+        costList = []
+        for it in range(max_iter):
+            if it % 1 == 0:
+                print("it:", it)
+            alphas = []
+            betas = []
+            gammas = []
+            Bs = []
+            P = np.zeros(N)
+
+            for n in range(N):
+                x = X[n]
+                T = len(x)
+
+                B = np.zeros((self.M, T))
+                component = np.zeros((self.M, self.K, T))
+                for j in range(self.M):
+                    for t in range(T):
+                        for k in range(self.K):
+                            p = self.R[j, k] * mvn.pdf(
+                                x=x[t],
+                                mean=self.mu[j, k],
+                                sigma=self.sigma[j, k]
+                            )
+                            component[j, k, t] = p
+                            B[j, t] += p
+                Bs.append(B)
+
+                alpha = np.zeros(shape=(T, self.M))
+                alpha[0] = self.pi * B[:, 0]
+
+    def fit(self, X):
         """
         Defines HMM parameters based on training data
         """
@@ -61,6 +106,9 @@ class HMMGMM:
 
         # initial HMM parameters
         self._setInitialParams(X=X)
+
+        # update HMM parameters
+        costList = self._setParams(X=X)
 
 
 if __name__ == "__main__":
