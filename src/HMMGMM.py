1e0b79d7b49ed75cd9ea12ae50c382aa7aa199bf
Darrell Leong <a843183@yara.com>
2021-08-11 18:33:40 +0800

    added sequence generation

--- src/HMMGMM.py
+++ src/HMMGMM.py
@@ -193,4 +193,56 @@ class HMMGMM:
 
 
 if __name__ == "__main__":
-    pass
+    def simple_init():
+        M = 1
+        K = 1
+        D = 1
+        pi = np.array([1])
+        A = np.array([[1]])
+        R = np.array([[1]])
+        mu = np.array([[[0]]])
+        sigma = np.array([[[[1]]]])
+        return M, K, D, pi, A, R, mu, sigma
+
+    def big_init():
+        M = 5
+        K = 3
+        D = 3
+        pi = np.array([1, 0, 0, 0, 0])
+        A = np.array([
+            [0.9, 0.025, 0.025, 0.025, 0.025],
+            [0.025, 0.9, 0.025, 0.025, 0.025],
+            [0.025, 0.025, 0.9, 0.025, 0.025],
+            [0.025, 0.025, 0.025, 0.9, 0.025],
+            [0.025, 0.025, 0.025, 0.025, 0.9],
+        ])
+        R = np.ones(shape=(M, K)) / K
+        mu = np.array([
+            [[0, 0], [1, 1], [2, 2]],
+            [[5, 5], [6, 6], [7, 7]],
+            [[10, 10], [11, 11], [12, 12]],
+            [[15, 15], [16, 16], [17, 17]],
+            [[20, 20], [21, 21], [22, 22]],
+        ])
+        sigma = np.zeros(shape=(M, K, D, D))
+        for m in range(M):
+            for k in range(K):
+                sigma[m, k] = np.eye(D)
+        return M, K, D, pi, A, R, mu, sigma
+
+    def get_signals(N=20, T=100, init=big_init):
+        M, K, D, pi, A, R, mu, sigma = init()
+        X = []
+        for n in range(N):
+            x = np.zeros(shape=(T, D))
+            s = 0
+            r = np.random.choice(a=K, p=R[s])
+            x[0] = np.random.multivariate_normal(mean=mu[s][r],
+                                                 cov=sigma[s][r])
+            for t in range(1, T):
+                s = np.random.choice(a=M, p=A[s])
+                r = np.random.choice(a=K, p=A[s])
+                x[t] = np.random.multivariate_normal(mean=mu[s][r],
+                                                     cov=sigma[s][r])
+            X.append(x)
+        return X
